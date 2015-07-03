51dc2d9b002192d65c4113045de39e35387444cc
Rajat Gupta <rajat.gupta712@gmail.com>
2015-07-03 09:03:53 +0530

    Added Isosceles Traingle Solution #05

---
+++ ISO_TRAINGLE.c
@@ -0,0 +1,22 @@
+/*
+ * Author: Rajat Gupta
+ */
+
+#include <stdio.h>
+
+#define read(n) scanf("%d", &n)
+
+int main() {
+    int n, i, j, k;
+    read(n);
+    for(i=1; i<=n; i++) {
+        for(j=1; j<=n-i; j++) {
+            printf(" ");
+        }
+        for(k=1; k<=(2*i)-1; k++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
