18cf4d8f4964f3216d495883d26e95258353263d
Rajat Gupta <rajat.gupta712@gmail.com>
2015-07-03 17:41:23 +0530

    Added horizontal line solution #02

---
+++ Java Solutions/H_LINE.java
@@ -0,0 +1,31 @@
+/*
+ * Author: Rajat Gupta
+ */
+
+import java.io.*;
+
+class Solution {
+    public static void main(String[] args) {
+        
+        String num = null;
+        int n;
+
+        try {
+            BufferedReader is = new BufferedReader(
+                    new InputStreamReader(System.in));
+            num = is.readLine();
+        } 
+        catch( IOException e) {
+            System.out.println("IOException: " + e);
+        }
+
+        n = Integer.parseInt(num);
+
+        while(n > 0) {
+            System.out.print("*");
+            n--;
+        }
+        System.out.print("\n");
+
+    }
+}
